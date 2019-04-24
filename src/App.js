f00aeb67e6b93792f4015045eb1d5d208b926d35
Mariah <skittles333dawn@gmail.com>
2019-04-24 00:07:08 -0400

    added json placeholder and routing

--- src/App.js
+++ src/App.js
@@ -5,30 +5,19 @@ import Header from './components/layout/Header';
 import Todos from './components/Todos';
 import AddTodo from './components/AddTodo';
 import About from './components/pages/About';
-import uuid from 'uuid';
+//import uuid from 'uuid';
+import axios from 'axios';
 
 
 class App extends Component{
    state = {
-    todos: [
-      {
-        id: uuid.v4(),
-        title: 'Take out the trash',
-        completed: false
-      },
-      {
-        id: uuid.v4(),
-        title: 'Dinner with wife',
-        completed: true
-      },
-      {
-        id: uuid.v4(),
-        title: 'Meeting with boss',
-        completed: false
-      }
-    ]
+    todos: []
   }
 
+  componentDidMount(){
+    axios.get('https://jsonplaceholder.typicode.com/todos?_limit=10')
+    .then(res => this.setState({todos: res.data}))
+  }
   //toggle complete
   markComplete = (id) => {
     this.setState({todos: this.state.todos.map(todo => {
@@ -41,16 +30,19 @@ class App extends Component{
 
   //delecte todo
   delTodo = (id) => {
-    this.setState({todos: [...this.state.todos.filter(todo => todo.id !== id)] });
+    axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
+    .then(res => this.setState({todos: [...this.state.todos.filter(todo => todo.id !== id)] }));
   }
 
   addTodo = (title) => {
-    const newTodo = {
-      id: uuid.v4(),
-      title,
-      completed: false
-    }
-    this.setState({todos: [...this.state.todos, newTodo]})
+    axios.post('https://jsonplaceholder.typicode.com/todos',{
+    title,
+    completed: false
+    })
+    .then(res => this.setState({
+      todos: [...this.state.todos, res.data]
+    }))
+    
   }
 
   render(){
--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,9 +1,10 @@
 import React from 'react';
-
+import {Link} from 'react-router-dom';
 function Header(){
     return(
         <header style={headerStyle}>
             <h1>TodoList</h1>
+            <Link style={linkStyle} to="/">Home</Link> | <Link style={linkStyle} to="/about">About</Link>
         </header>
     )
 }
@@ -14,5 +15,8 @@ const headerStyle = {
     textAlign: 'center',
     padding: '10px'
 }
-
+const linkStyle = {
+    color: '#fff',
+    textDecoration: 'none'
+}
 export default Header;
